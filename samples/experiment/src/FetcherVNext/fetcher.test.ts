4e133486384a55bc14e0959520bdd5aed496feb6
Patrick Roza <contact@patrickroza.com>
2020-03-07 17:10:51 +0100

    More test

--- samples/experiment/src/FetcherVNext/fetcher.test.ts
+++ samples/experiment/src/FetcherVNext/fetcher.test.ts
@@ -5,11 +5,30 @@ import * as TE from "fp-ts/lib/TaskEither"
 import * as t from "io-ts"
 import { failure } from "io-ts/lib/PathReporter"
 
-import { Decoder, extend, Fetcher, handleError, jsonDecoder, make } from "./fetcher"
+import fetch from "cross-fetch"
+
+import {
+  Decoder,
+  extend,
+  Fetcher,
+  handleError,
+  jsonDecoder,
+  make,
+  toTaskEither,
+} from "./fetcher"
 
 const User = t.type({ name: t.string })
 const Users = t.array(User)
 const FourTwoTwo = t.type({ code: t.number, correlationId: t.string })
+
+const Todo = t.type({
+  userId: t.number,
+  id: t.number,
+  title: t.string,
+  completed: t.boolean,
+})
+
+type Todo = t.TypeOf<typeof Todo>
 
 type User = t.TypeOf<typeof User>
 type FourTwoTwo = t.TypeOf<typeof FourTwoTwo>
@@ -21,7 +40,7 @@ type GetUserResult =
   | { code: 422; payload: FourTwoTwo }
 
 // helpers:
-it("works", () => {
+it("works", async () => {
   const decodeError = (errors: t.Errors): Error => new Error(failure(errors).join("\n"))
 
   const handleUsers = (res: Response) =>
@@ -49,7 +68,7 @@ it("works", () => {
     pipe(
       headersDecoder(res),
       TE.chain(
-        pipe(
+        flow(
           FourTwoTwo.decode,
           E.bimap(decodeError, payload => ({ code: 422 as const, payload })),
           TE.fromEither,
@@ -84,14 +103,43 @@ it("works", () => {
     100: () => TE.left<Error, GetUserResult>(new Error("aaaaaaaa")),
   })(fetcher2)
 
-  const fetcher3 = make<200 | 422 | 400, Error, GetUserResult>(
-    "myurl",
+  type TodoResult =
+    | {
+        code: 200
+        payload: Todo
+      }
+    // | { code: 400; payload: Error }
+    // | { code: 401; payload: [Error, string] }
+    | { code: 422; payload: FourTwoTwo }
+
+  const handleTodo = (res: Response) =>
+    pipe(
+      jsonDecoder(res),
+      TE.chain(
+        flow(
+          Todo.decode,
+          E.bimap(decodeError, payload => ({ code: 200 as const, payload })),
+          TE.fromEither,
+        ),
+      ),
+    )
+
+  const fetcher3 = make<200 | 422, Error, TodoResult>(
+    "https://jsonplaceholder.typicode.com/todos/1",
     {
-      200: handleUsers,
+      200: handleTodo,
       422: handleFourTwoTwo,
     }, // => Property '400' is missing in type ...
-    () => E.left("unexpected error"),
+    () => TE.left(new Error("unexpected error")),
   )
+
+  const fetchR = toTaskEither(fetch)
+
+  const fetchMe = fetchR(fetcher3)
+
+  const result = await fetchMe()
+  expect(result._tag).toBe("Right")
+  expect(result.right.code).toBe(200)
 
   expect(1).toBe(1)
 })
