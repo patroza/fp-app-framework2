def9c69d21746f71a74c32e51f36a3126a661025
Patrick Roza <contact@patrickroza.com>
2020-03-16 13:03:21 +0100

    Remove log

--- samples/basic/src/TrainTrip/infrastructure/utils.ts
+++ samples/basic/src/TrainTrip/infrastructure/utils.ts
@@ -4,9 +4,8 @@ import { pipe } from "fp-ts/lib/pipeable"
 
 export const wrap = <T>(
   f: RT.ReaderTask<string, O.Option<T>>,
-): RTE.ReaderTaskEither<string, RecordNotFound, T> => (id: string) => {
-  console.log(id)
-  return pipe(
+): RTE.ReaderTaskEither<string, RecordNotFound, T> => (id: string) =>
+  pipe(
     f(id),
     T.map(x => {
       if (O.isSome(x)) {
@@ -15,4 +14,3 @@ export const wrap = <T>(
       return E.err(new RecordNotFound("TrainTrip", id))
     }),
   )
-}
