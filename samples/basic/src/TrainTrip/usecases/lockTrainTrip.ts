38d9926e85bdcf392b6882e2ac389da6d1d56943
Patrick Roza <contact@patrickroza.com>
2020-03-16 22:42:17 +0100

    sample convert lock

--- samples/basic/src/TrainTrip/usecases/lockTrainTrip.ts
+++ samples/basic/src/TrainTrip/usecases/lockTrainTrip.ts
@@ -2,6 +2,8 @@ import { createCommandWithDeps, DbError } from "@fp-app/framework"
 import { TE } from "@fp-app/fp-ts-extensions"
 import { DbContextKey, defaultDependencies } from "./types"
 import { wrap } from "../infrastructure/utils"
+import { lock } from "../TrainTrip"
+import { tuple } from "fp-ts/lib/function"
 
 const createCommand = createCommandWithDeps({
   db: DbContextKey,
@@ -14,7 +16,11 @@ const lockTrainTrip = createCommand<Input, void, LockTrainTripError>(
     TE.compose(
       TE.map(({ trainTripId }) => trainTripId),
       TE.chain(wrap(db.trainTrips.load)),
-      TE.map(trainTrip => trainTrip.lock()),
+      TE.map(trainTrip => tuple(trainTrip, ...lock(trainTrip)())),
+      TE.map(([trainTrip, newTT, events]) => {
+        Object.assign(trainTrip, newTT)
+        events.forEach(trainTrip.registerDomainEvent)
+      }),
     ),
 )
 
