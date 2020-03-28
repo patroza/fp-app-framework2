e55cfdec05a163c6a3bbda9f3ad416113d89849e
Patrick Roza <contact@patrickroza.com>
2020-03-28 17:43:05 +0100

    Type cleanup for framework

--- packages/framework/src/infrastructure/mediator/request.ts
+++ packages/framework/src/infrastructure/mediator/request.ts
@@ -1,7 +1,7 @@
 import {
   NamedHandlerWithDependencies,
-  NamedRequestHandler,
   requestType,
+  NamedRequestHandler,
 } from "./registry"
 
 const request = (get: getRequestType): requestType => (requestHandler, input) => {
@@ -12,5 +12,6 @@ const request = (get: getRequestType): requestType => (requestHandler, input) =>
 export default request
 
 type getRequestType = <TInput, TOutput, TError>(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   key: NamedHandlerWithDependencies<any, TInput, TOutput, TError>,
 ) => NamedRequestHandler<TInput, TOutput, TError>
