4115d07a748d8af4efaeb36f9f4093ba2fc6126c
Patrick Roza <contact@patrickroza.com>
2020-04-04 23:41:02 +0200

    Use Morphic for one object for testing. (#18)
    * mmmmmorphic
    
    * Cleanup a bit.
    
    * wishful
    
    * Update to fixed version
    
    * Apply updated usage of F.refined.
    
    * Fix interface
    
    * Fix local builds
    
    * Bump deps

--- samples/basic/src/TrainTrip/PaxDefinition.ts
+++ samples/basic/src/TrainTrip/PaxDefinition.ts
@@ -1,19 +1,20 @@
-//import { summon } from "@morphic-ts/batteries/lib/summoner"
+import { summon } from "@morphic-ts/batteries/lib/summoner-ESBASTJ"
+import { iotsConfig } from "@morphic-ts/io-ts-interpreters/lib"
 
 import { typedKeysOf, ValidationError } from "@fp-app/framework"
 import { t, withBla, decodeErrors } from "@fp-app/fp-ts-extensions"
-import { PositiveInt, merge } from "@fp-app/fp-ts-extensions/src/Io"
+import { merge } from "@fp-app/fp-ts-extensions/src/Io"
 import { flow } from "fp-ts/lib/function"
-import { map, mapLeft } from "@fp-app/fp-ts-extensions/src/Either"
+import { map, mapLeft, either } from "@fp-app/fp-ts-extensions/src/Either"
 
 /* Pax: No domain validation, just primitives. **/
 const Pax = t.type(
   {
-    adults: t.Int,
-    babies: t.Int,
-    children: t.Int,
-    infants: t.Int,
-    teenagers: t.Int,
+    adults: t.number,
+    babies: t.number,
+    children: t.number,
+    infants: t.number,
+    teenagers: t.number,
   },
   "Pax",
 )
@@ -27,86 +28,113 @@ export interface PaxNumberBrand {
   readonly PaxNumber: unique symbol // use `unique symbol` here to ensure uniqueness across modules / packages
 }
 
-const PaxNumber = withBla(
-  t.brand(
-    PositiveInt, // a codec representing the type to be refined
-    (n): n is t.Branded<PositiveInt, PaxNumberBrand> => n >= 0 && n <= 6, // a custom type guard using the build-in helper `Branded`
-    "PaxNumber", // the name must match the readonly field in the brand
+const PaxNumber = summon((F) =>
+  F.refined(
+    F.number(),
+    (n): n is t.Branded<number, PaxNumberBrand> => n >= 0 && n <= 6,
+    "PaxNumber",
+    iotsConfig((x) =>
+      withBla(x, (value) => {
+        return `requires a number between 0 and max 6, but ${value} was specified.`
+      }),
+    ),
   ),
-  (value) => {
-    if (!PositiveInt.is(value)) {
-      return "Invalid input"
-    }
-    return `requires between 0 and max 6, but ${value} was specified.`
-  },
 )
 
-export type PaxNumber = t.TypeOf<typeof PaxNumber>
-
-const PaxFields = t.type(
-  {
-    adults: PaxNumber,
-    babies: PaxNumber,
-    children: PaxNumber,
-    infants: PaxNumber,
-    teenagers: PaxNumber,
-  },
-  "PaxFields",
-)
-
-type PaxFieldsType = t.TypeOf<typeof PaxFields>
+type PaxNumber = t.TypeOf<typeof PaxNumber.type>
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface PaxFields extends PaxFieldsType {}
 
-export interface PaxDefinitionBrand {
-  readonly PaxDefinition: unique symbol // use `unique symbol` here to ensure uniqueness across modules / packages
-}
+const _PaxDefinition = summon((F) => {
+  type T = {
+    adults: PaxNumber
+    babies: PaxNumber
+    children: PaxNumber
+    infants: PaxNumber
+    teenagers: PaxNumber
+  }
+  const validatePax = (p: T) =>
+    typedKeysOf(p).some((k) => p[k] > 0) &&
+    typedKeysOf(p).reduce((prev, cur) => (prev += p[cur]), 0) <= 6
 
-const _PaxDefinition = withBla(
-  t.brand(
-    PaxFields, // a codec representing the type to be refined
-    (p): p is t.Branded<PaxFields, PaxDefinitionBrand> =>
-      typedKeysOf(p).some((k) => p[k] > 0) &&
-      typedKeysOf(p).reduce((prev, cur) => (prev += p[cur]), 0) <= 6, // a custom type guard using the build-in helper `Branded`
-    "PaxDefinition", // the name must match the readonly field in the brand
-  ),
-  (value) => {
-    if (!PaxFields.is(value)) {
-      return "Invalid input"
-    }
-    return `requires at least 1 and max 6 people, but ${typedKeysOf(value).reduce(
-      (prev, cur) => (prev += value[cur]),
-      0,
-    )} were specified`
-  },
-)
+  return F.interface(
+    {
+      adults: PaxNumber(F),
+      babies: PaxNumber(F),
+      children: PaxNumber(F),
+      infants: PaxNumber(F),
+      teenagers: PaxNumber(F),
+    },
+    "Pax",
+    iotsConfig((x) =>
+      withBla(
+        new t.Type(
+          "PaxDefinition",
+          (p2): p2 is T => {
+            const p = p2 as T
+            return validatePax(p)
+          },
+          (u, c) =>
+            either.chain(x.validate(u, c), (v) =>
+              validatePax(v) ? t.success(v) : t.failure(u, c),
+            ),
+          x.encode,
+        ),
+        (value: T) => {
+          return `requires at least 1 and max 6 people, but ${typedKeysOf(value).reduce(
+            (prev, cur) => (prev += value[cur]),
+            0,
+          )} were specified`
+        },
+      ),
+    ),
+  )
+})
 
 const PaxDefinition = merge(_PaxDefinition, {
   create: flow(
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    _PaxDefinition.decode,
+    _PaxDefinition.type.decode,
     map((x) => x as PaxDefinition),
     mapLeft((x) => new ValidationError(decodeErrors(x))),
   ),
 })
 
-type PaxDefinitionType = t.TypeOf<typeof _PaxDefinition>
+type PaxDefinitionType = t.TypeOf<typeof _PaxDefinition.type>
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface PaxDefinition extends PaxDefinitionType {}
 
 export default PaxDefinition
 
-// COmpiler gives some errors about PURI and unique symbol etc..
-// const Pax = summon(F =>
-//   F.interface(
-//     {
-//       adults: F.number(),
-//       babies: F.number(),
-//       children: F.number(),
-//       infants: F.number(),
-//       teenagers: F.number(),
-//     },
-//     "Person",
-//   ),
+// console.log(PaxDefinition.type.decode({ adults: 1 }))
+
+// console.log(PaxNumber.type.decode(9))
+
+// console.log(
+//   PaxDefinition.type.decode({
+//     adults: 7,
+//     babies: 1,
+//     children: 1,
+//     infants: 0,
+//     teenagers: 2,
+//   }).left[0],
 // )
-// export type Pax = t.TypeOf<typeof Pax.type>
+
+// console.log(
+//   PaxDefinition.type.decode({
+//     adults: 3,
+//     children: 3,
+//     babies: 0,
+//     teenagers: 0,
+//     infants: 0,
+//   }),
+// )
+
+// console.log(
+//   PaxDefinition.type.decode({
+//     adults: 5,
+//     children: 5,
+//     babies: 0,
+//     teenagers: 0,
+//     infants: 0,
+//   }),
+// )
