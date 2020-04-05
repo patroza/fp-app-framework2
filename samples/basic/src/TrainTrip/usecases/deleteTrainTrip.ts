18f4a3c39bcf7916c5da14d7cf64e09e5d188965
Patrick Roza <contact@patrickroza.com>
2020-04-05 15:55:04 +0200

    Simplify delete.

--- samples/basic/src/TrainTrip/usecases/deleteTrainTrip.ts
+++ samples/basic/src/TrainTrip/usecases/deleteTrainTrip.ts
@@ -1,8 +1,7 @@
 import { createCommandWithDeps, DbError } from "@fp-app/framework"
-import { pipe } from "@fp-app/fp-ts-extensions"
+import { pipe, Do, TE } from "@fp-app/fp-ts-extensions"
 import { defaultDependencies } from "./types"
 import { wrap } from "../infrastructure/utils"
-import { compose, map, chain } from "@fp-app/fp-ts-extensions/src/TaskEither"
 import { trainTrips } from "@/TrainTrip/infrastructure/TrainTripContext.disk"
 
 const createCommand = createCommandWithDeps(() => ({
@@ -12,16 +11,15 @@ const createCommand = createCommandWithDeps(() => ({
 
 const deleteTrainTrip = createCommand<Input, void, DeleteTrainTripError>(
   "deleteTrainTrip",
-  ({ _, trainTrips }) =>
-    compose(
-      map(({ trainTripId }) => trainTripId),
-      chain(pipe(wrap(trainTrips.load), _.RTE.liftErr)),
-      // TODO: this should normally be on a different object.
-      map((x) => {
-        x.delete()
-        trainTrips.remove(x)
-      }),
-    ),
+  ({ _, trainTrips }) => (input) =>
+    Do(TE.taskEither)
+      .bind(
+        "trainTrip",
+        pipe(input.trainTripId, pipe(wrap(trainTrips.load), _.RTE.liftErr)),
+      )
+      .letL("_", ({ trainTrip }) => trainTrip.delete())
+      .letL("__", ({ trainTrip }) => trainTrips.remove(trainTrip))
+      .return(() => void 0 as void),
 )
 
 export default deleteTrainTrip
