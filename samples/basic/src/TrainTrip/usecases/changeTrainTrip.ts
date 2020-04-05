4831fa510c673349b596dea403e2011f489e7303
Patrick Roza <contact@patrickroza.com>
2020-04-05 13:06:47 +0200

    Play with Do notation.

--- samples/basic/src/TrainTrip/usecases/changeTrainTrip.ts
+++ samples/basic/src/TrainTrip/usecases/changeTrainTrip.ts
@@ -8,7 +8,7 @@ import {
   ValidationError,
   FieldValidationError,
 } from "@fp-app/framework"
-import { pipe, E, NA, t } from "@fp-app/fp-ts-extensions"
+import { pipe, E, NA, t, TE } from "@fp-app/fp-ts-extensions"
 import { trainTrips } from "@/TrainTrip/infrastructure/TrainTripContext.disk"
 import FutureDate from "../FutureDate"
 import PaxDefinition, { Pax } from "../PaxDefinition"
@@ -18,7 +18,6 @@ import { Do } from "fp-ts-contrib/lib/Do"
 import { getMonoid } from "fp-ts/lib/Array"
 import { wrap } from "../infrastructure/utils"
 import { flow } from "fp-ts/lib/function"
-import { compose, chain, chainTup, map } from "@fp-app/fp-ts-extensions/src/TaskEither"
 
 const createCommand = createCommandWithDeps(() => ({
   trainTrips,
@@ -27,34 +26,33 @@ const createCommand = createCommandWithDeps(() => ({
 
 const changeTrainTrip = createCommand<Input, void, ChangeTrainTripError>(
   "changeTrainTrip",
-  ({ _, trainTrips }) =>
-    compose(
-      chain(pipe(validateStateProposition, _.RE.liftErr, E.toTaskEither)),
-      chainTup(
-        compose(
-          map((i) => i.trainTripId),
-          chain(pipe(wrap(trainTrips.load), _.RTE.liftErr)),
-        ),
-      ),
-      chain(([trainTrip, proposal]) =>
-        pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither, (f) =>
-          f(proposal),
-        ),
-      ),
-      // ALT1
-      // compose(
-      //   map(
-      //     ([trainTrip, proposal]) =>
-      //       tuple(pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither), proposal),
-      //   ),
-      //   chain(([proposeChanges, trainTripId]) => proposeChanges(trainTripId)),
-      // ),
-      // ALT2
-      //{
-      //  const proposeChanges = pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither)
-      //  return proposeChanges(proposal)
-      //}
-    ),
+  ({ _, trainTrips }) => (input) =>
+    Do(TE.taskEither)
+      .bind(
+        "proposal",
+        pipe(validateStateProposition, _.RE.liftErr, E.toTaskEither)(input),
+      )
+      .bindL("trainTrip", ({ proposal }) =>
+        pipe(wrap(trainTrips.load), _.RTE.liftErr)(proposal.trainTripId),
+      )
+      .doL(({ proposal, trainTrip }) =>
+        pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither)(proposal),
+      )
+      .return(() => void 0 as void),
+
+  // ALT1
+  // compose(
+  //   map(
+  //     ([trainTrip, proposal]) =>
+  //       tuple(pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither), proposal),
+  //   ),
+  //   chain(([proposeChanges, trainTripId]) => proposeChanges(trainTripId)),
+  // ),
+  // ALT2
+  //{
+  //  const proposeChanges = pipe(trainTrip.proposeChanges, _.RE.liftErr, E.toTaskEither)
+  //  return proposeChanges(proposal)
+  //}
 )
 export default changeTrainTrip
 
