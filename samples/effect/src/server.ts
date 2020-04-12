590f90ee413f58a260019dc64580768e11056554
Patrick Roza <contact@patrickroza.com>
2020-04-12 16:16:54 +0200

    Add request info MW.

--- samples/effect/src/server.ts
+++ samples/effect/src/server.ts
@@ -3,6 +3,7 @@ import * as KOA from "@matechs/koa"
 // import { Do } from "fp-ts-contrib/lib/Do"
 import { sequenceT } from "fp-ts/lib/Apply"
 import trainTripR from "@/TrainTrip/router"
+import { utils } from "@fp-app/framework"
 
 const getHelloWorldRoute = KOA.route(
   "get",
@@ -16,13 +17,34 @@ const getHelloWorldRoute = KOA.route(
 
 const mainR = sequenceT(T.effect)(getHelloWorldRoute)
 
-const program = sequenceT(T.effect)(
-  mainR,
-  trainTripR,
-  KOA.middleware((ctx, next) => {
-    ctx.set("X-Request-Id", "my-id")
-    return next()
-  }),
-)
+interface RequestInfo {
+  id: string
+  path: string
+  startedAt: Date
+  finishedAt?: Date
+}
+const requestInfoMW = KOA.middleware((ctx, next) => {
+  const requestContext: RequestInfo = {
+    id: ctx.get("X-Request-Id") || utils.generateShortUuid(),
+    path: ctx.originalUrl,
+    startedAt: getCurrentDate(),
+  }
+  ctx.set("X-Request-Id", requestContext.id)
+  ctx.set("X-Request-StartedAt", requestContext.startedAt.toISOString())
+  console.log("starting request", JSON.stringify(requestContext, undefined, 2))
+  return next().then(() => {
+    requestContext.finishedAt = getCurrentDate()
+    ctx.set("X-Request-FinishedAt", requestContext.finishedAt.toISOString())
+    console.log("finishing request", JSON.stringify(requestContext, undefined, 2))
+  })
+})
+
+const middlewares = sequenceT(T.effect)(requestInfoMW)
+
+const program = sequenceT(T.effect)(mainR, trainTripR, middlewares)
 
 export default program
+
+function getCurrentDate() {
+  return new Date()
+}
