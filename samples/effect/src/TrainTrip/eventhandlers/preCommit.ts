9fcb0dd80b1d256e8067b9237b08383c8c0d9315
Patrick Roza <contact@patrickroza.com>
2020-04-15 21:36:39 +0200

    Cleanup

--- samples/effect/src/TrainTrip/eventhandlers/preCommit.ts
+++ samples/effect/src/TrainTrip/eventhandlers/preCommit.ts
@@ -4,27 +4,6 @@ import * as TC from "@e/TrainTrip/infrastructure/TrainTripContext.disk"
 import * as API from "@e/TrainTrip/infrastructure/api"
 import { T } from "@e/meffect"
 
-// import { U } from "ts-toolbelt"
-
-// const readInAggregateTodosOnlyTodoRemoved = todosAggregate.readOnly(
-//     defaultConfig("read-todo-removed")
-//   )(["TodoRemoved"])(match =>
-//     match({
-//       TodoRemoved: todoRemoved => logger.info(JSON.stringify(todoRemoved))
-//     })
-//   );
-
-// const handleEvents = <R, E, A>(
-//   match: (handlers: {
-//     OnTrainTripStateChanged: <R, E, A>(
-//       event: TrainTripStateChanged,
-//     ) => T.Effect<R, E, A>
-//   }) => T.Effect<R, E, A>,
-// ) =>
-//   match({
-//     OnTrainTripStateChanged,
-//   })
-
 const OnTrainTripStateChanged = (event: TrainTripStateChanged) =>
   Do(T.effect)
     .bind("trainTrip", TC.loadE(event.trainTripId))
@@ -62,24 +41,3 @@ export const handlers = (evt: Events) => {
     return notImplemented(evt)
   }
 }
-
-// createDomainEventHandler<TrainTripStateChanged, void, RefreshTripInfoError>(
-//     /* on */ TrainTripStateChanged,
-//     "RefreshTripInfo",
-//     ({ _, getTrip, trainTrips }) => (event) =>
-//       Do(TE.taskEither)
-//         .bind(
-//           "trainTrip",
-//           pipe(event.trainTripId, pipe(wrap(trainTrips.load), _.RTE.liftErr)),
-//         )
-//         .bindL("trip", ({ trainTrip }) =>
-//           pipe(
-//             trainTrip.currentTravelClassConfiguration.travelClass.templateId,
-//             pipe(getTrip, _.RTE.liftErr),
-//           ),
-//         )
-//         .doL(({ trainTrip, trip }) => pipe(trainTrip.updateTrip(trip), TE.right))
-//         .return(toVoid),
-//   )
-
-// type RefreshTripInfoError = DbError | ApiError | InvalidStateError
