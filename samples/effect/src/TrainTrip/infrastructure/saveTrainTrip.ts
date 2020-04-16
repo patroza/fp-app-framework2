be7bf8e035f21ae45e3d9161940ff16880c360af
Patrick Roza <contact@patrickroza.com>
2020-04-16 17:55:00 +0200

    Remove preCommit accordingly.

--- samples/effect/src/TrainTrip/infrastructure/saveTrainTrip.ts
+++ samples/effect/src/TrainTrip/infrastructure/saveTrainTrip.ts
@@ -1,7 +1,6 @@
 import TrainTrip, { Events } from "../TrainTrip"
 import { Do } from "fp-ts-contrib/lib/Do"
 import { T } from "@e/meffect"
-import * as PreCommit from "../eventhandlers/preCommit"
 import * as PostCommit from "../eventhandlers/postCommit"
 import { sequenceT } from "fp-ts/lib/Apply"
 import * as TC from "@e/TrainTrip/infrastructure/TrainTripContext.disk"
@@ -20,8 +19,6 @@ export const save = <TEvents extends readonly Events[]>(
         ? TC.add(tt)
         : TC.registerChanged(tt),
     )
-    // eventHandlers should update records accordingly
-    .do(handleEvents(events))
     .do(TC.save())
     .do(handlePostCommitEvents(events))
     .bind("updatedTrainTrip", method === "delete" ? T.pure(tt) : TC.loadE(tt.id))
@@ -30,14 +27,6 @@ export const save = <TEvents extends readonly Events[]>(
 export default save
 
 export const saveT = tupled(save)
-
-const handleEvents = <TEvents extends readonly Events[]>(events: TEvents) =>
-  events.length
-    ? sequenceT(T.effect)(
-        PreCommit.handlers(events[0]),
-        ...events.slice(1).map((x) => PreCommit.handlers(x)),
-      )
-    : T.unit
 
 const handlePostCommitEvents = <TEvents extends readonly Events[]>(events: TEvents) =>
   events.length
