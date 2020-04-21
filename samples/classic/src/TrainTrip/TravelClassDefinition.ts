2fe28958eeefba9e12419abaf6578da1213c3c89
Patrick Roza <contact@patrickroza.com>
2020-04-21 20:05:04 +0200

    r/morphic110 (#26)
    * Morphic 1.1.0 upgrade - but fails on iotsconfig.
    
    * Fix morphic use
    
    * Bump packages
    
    * Upgrade deps

--- samples/classic/src/TrainTrip/TravelClassDefinition.ts
+++ samples/classic/src/TrainTrip/TravelClassDefinition.ts
@@ -1,9 +1,23 @@
-import { summon } from "@morphic-ts/batteries/lib/summoner-ESBASTJ"
 import { AType } from "@morphic-ts/batteries/lib/usage/utils"
 import { ValidationError } from "@fp-app/framework"
 import { pipe } from "@fp-app/fp-ts-extensions"
 import { merge } from "@fp-app/fp-ts-extensions/src/Io"
 import { mapLeft } from "@fp-app/fp-ts-extensions/src/Either"
+
+import { summonFor } from "@morphic-ts/batteries/lib/summoner-ESBASTJ"
+
+import { IoTsURI } from "@morphic-ts/io-ts-interpreters/lib"
+import { withMessage as WM } from "io-ts-types/lib/withMessage"
+
+export const { summon } = summonFor<{
+  [IoTsURI]: IoTsEnv
+}>({
+  [IoTsURI]: { WM },
+})
+
+interface IoTsEnv {
+  WM: typeof WM
+}
 
 const _TravelClassDefinition = summon((F) =>
   F.keysOf({ first: null, second: null, business: null }),
